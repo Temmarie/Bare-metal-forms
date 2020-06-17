992291a766f12f735aaec1a6f7e6845370e5dcd0
Temmarie <temmar_whyte16@yahoo.com>
2020-06-17 11:16:24 +0100

    fixed linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-
-    validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { within: 6..40 }
-
 end
