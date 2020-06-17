cf1ce627be96230888f6a5421168e5c7e7f87151
Temmarie <temmar_whyte16@yahoo.com>
2020-06-17 10:52:20 +0100

    added more functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
     def new
+      @user = User.new
     end
 
     def create
+      # @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
         @user = User.new(user_params)
       if @user.save
         redirect_to new_user_path
@@ -11,7 +13,9 @@ class UsersController < ApplicationController
         render :new
       end
     end
+
     private
+
     def user_params
         params.require(:user).permit(:username, :email, :password)
       end
